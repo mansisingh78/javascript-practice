636cd006a2a88e9decaa2eb3f7218cd58b336bb0
Mansi singh <mansi123singh1234@gmail.com>
2024-06-12 10:03:49 +0530

    third question complete

--- script.js
+++ script.js
@@ -13,3 +13,17 @@ function isSportsCar(car) {
 }
 console.log(isSportsCar(car1));
 console.log(isSportsCar(car2));
+
+// 3. Write a function that takes an object person and a number num as arguments and returns true if
+// the person's age plus num is greater than 21. Otherwise, it should return false.
+
+const person1 = { name: "Ajay", age: 20 };
+
+function isGreaterThan21(num, person) {
+  if (person.age + num > 21) {
+    return true;
+  } else {
+    return false;
+  }
+}
+console.log(isGreaterThan21(23, person1));
